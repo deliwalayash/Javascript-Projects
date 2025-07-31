c744ffd997a0a78032e82a721c7c8e7693a1afac
deliwalayash <deliwalayash2@gmail.com>
2025-07-31 10:04:51 +0530

    some changes made

--- haker-rank/1.js
+++ haker-rank/1.js
@@ -7,22 +7,21 @@
 //     console.log(`Step ${step}: Jumped to index ${i}`);
 // }
 
+// let str1 = "yash";
+// let str2 = "deliwala";
+// let newStr = "";
+// let i=0;
+// let j=0;
+// while(i<str1.length || j<str2.length){
+//     if(i<str1.length){
+//         newStr += str1[i++]
+//     }
+//     if(j<str2.length){
+//         newStr += str2[j++]
+//     }
 
-let str1 = "yash";
-let str2 = "deliwala";
-let newStr = "";
-let i=0;
-let j=0;
-while(i<str1.length || j<str2.length){
-    if(i<str1.length){
-        newStr += str1[i++]
-    }
-    if(j<str2.length){
-        newStr += str2[j++]
-    }
-
-}
-console.log(newStr);
+// }
+// console.log(newStr);
 
 
 // let newStr = [];
@@ -41,4 +40,45 @@ console.log(newStr);
 // newStr = newStr.join("");
 
 // console.log(newStr);
+
+
+// find pair of socks 
+// let arr = [10, 10, 0, 0, 10,0, 30, 40, 50, 50, 50]
+// // output = 3
+// let group= 0;
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = (i+1); j < arr.length; j++){
+//         if(arr[i] == arr[j]){
+//             console.log(arr[i], arr[j]);
+//             group++;
+//             arr[i] = null
+//             arr[j] = null
+//         }
+//     }
+// }
+// console.log(group);
+
+let arr = [5, 4, 4, 2, 2, 8];
+let ans = []
+
+
+// output [ 6, 4, 2, 1 ]
+
+
+while (arr.length) {
+    let min;
+    for (let i = 0; i < arr.length; i++) {
+        min = arr[i]
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+    }
+    arr = arr.map((ele) => {
+        if (ele - min !== 0) {
+            return ele - min
+        }
+    })
+}
+console.log(arr);
 
