f7f9647ae404c1bdf6333193ca8b30841bed8609
deliwalayash <deliwalayash2@gmail.com>
2025-08-06 11:04:45 +0530

    timing function done

--- haker-rank/keprekar.js
+++ haker-rank/keprekar.js
@@ -1,22 +1,32 @@
-let num=99
-
-let numstr=num.toString().split("")
-
-let numlength=numstr.length
-
-
-let square=num*num
-
-let squarestr=square.toString()
-
-let squarestrlength=squarestr.length
-
-
-let lr=squarestr.slice(0,numlength)
-let rr=squarestr.slice(2,squarestrlength)
-
-let sum2=parseInt(lr)+parseInt(rr)
-
-console.log(sum2)
-
-
+let arr=[]
+    
+    for(let  i = p;i <= q ;i++){
+        
+        if(i ==1){
+            arr.push(i)
+        }
+        let numlength=i.toString().length
+        
+        let mul=i*i
+        
+        let mullength=mul.toString().length
+        
+        let mulstr=mul.toString()
+        
+        let ls=mulstr.slice(0,mullength - numlength)
+        
+        let rs=mulstr.slice(mullength - numlength)
+        
+        let sum=parseInt(ls)+ parseInt(rs)
+        
+        if (sum == i){
+            arr.push(i)
+        }
+    }
+    
+    if (arr.length == 0){
+        console.log("INVALID RANGE")
+        return 
+    }
+    console.log(arr.join(" "))
+   
