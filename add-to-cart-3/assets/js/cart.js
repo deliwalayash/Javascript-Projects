5fa641291af0b2cefb348090221b534223cb56b7
deliwalayash <deliwalayash2@gmail.com>
2025-09-03 17:53:18 +0530

    search function added to add to cart

---
+++ add-to-cart-3/assets/js/cart.js
@@ -0,0 +1,73 @@
+let counter=document.getElementById("counter")
+let clearBtn=document.getElementById("clearBtn")
+let empty=document.getElementById("empty")
+let cartTable=document.getElementById("cartTable")
+let cartItem=document.getElementById("cartItem")
+let total=document.getElementById("mainTotal")
+
+let cart=JSON.parse(localStorage.getItem("cart")) || []
+
+function deleteitem(idx){
+    cart.splice(idx,1)
+    updateCart()
+
+}
+
+function updateCart(){
+    localStorage.setItem("cart",JSON.stringify(cart))
+    displayCart()
+}
+
+function updateQuantity(idx,value){
+
+    let updatedQuantity=cart[idx].quantity + value
+
+    if(updatedQuantity <=0){
+        deleteitem(idx)
+    }
+    else{
+        cart[idx].quantity = updatedQuantity
+    }
+
+    updateCart()
+}
+
+function displayCart(){
+
+    let mainTotal=0
+    if(cart.length <=0){
+    empty.classList.remove("d-none")
+    mainTotal=0
+    counter.innerHTML=0
+    total.innerHTML=mainTotal
+     cartTable.classList.add("d-none")
+    return
+    }
+
+    cartTable.classList.remove("d-none")
+    cartItem.innerHTML=""
+    
+    cart.forEach((product,idx)=>{
+        let subTotal=product.price * product.quantity
+        mainTotal +=subTotal
+        cartItem.innerHTML += `
+        <tr>
+        <td><img class="img fluid" src="${product.image}"<td>
+        <td><h4>${product.heading}</h4></td>
+        <td><h4>$${product.price}</h4></td>
+        <td><h4>${product.quantity}</h4></td>
+        <td><h4>$${subTotal}</h4></td>
+        <td>
+        <button class="btn btn-warning" onclick="updateQuantity(${idx},-1)">-</button>
+        <button class="btn btn-danger" onclick="deleteitem(${idx})">🗑</button>
+        <button class="btn btn-warning" onclick="updateQuantity(${idx},1)">+</button>
+        </td>
+
+        </tr>
+      `
+    })
+    total.innerHTML=mainTotal
+
+}
+
+displayCart()
---
+++ add-to-cart-3/assets/js/index.js
@@ -0,0 +1,207 @@
+let procuctlist=[{
+            id:1,
+            image:"https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(1).png&w=828&q=75",
+            heading:"Grey Sofa",
+            rating:"3.5",
+            color:"Black Blue Grey",
+            price:"138.70",
+            delprice:"150"
+            
+        },
+        {
+            id:2,
+            image:"https://bonik-react.vercel.app/assets/images/Furniture%20Shop/Furniture%20(2).png",
+            heading:"Black Sofa",
+            rating:"4.0",
+            color:"Black Blue Grey",
+            price:"200",
+            delprice:"250"
+
+        },
+        {
+            id:3,
+            image:"https://bonik-react.vercel.app/assets/images/Furniture%20Shop/Furniture%20(3).png",
+            heading:"Comfort Sofa",
+            rating:"2.0",
+            color:"Black Blue Grey",
+            price:"200",
+
+        },
+        {
+            id:4,
+            image:"https://thetimberguy.com/cdn/shop/products/Mid-Century-designer-wooden-rattan-cane-sofa-with-cushion-Make-your-combination_1200x.jpg?v=1680545367",
+            heading:"Study Sofa",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"200",
+
+        },
+        {
+            id:5,
+            image:"https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2FFurniture%20Shop%2FFurniture%20(4).png&w=640&q=75",
+            heading:"Study Chair",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"300",
+
+        },
+        {
+            id:6,
+            image:"https://craftsmill.in/cdn/shop/files/sofas-accent-chairs-cider-orange-soft-velvet-touch-fabric-emily-flared-arm-2-seater-sofa-60-46567514931491.jpg?v=1725047510",
+            heading:"Sofa",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"300",
+
+        },
+        {
+            id:7,
+            image:"https://img.freepik.com/premium-psd/kitchen-table-isolated-transparent-background_191095-13975.jpg?semt=ais_incoming&w=740&q=80",
+            heading:"Dining Table",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"250",
+
+        },
+        {
+            id:8,
+            image:"https://png.pngtree.com/png-clipart/20231129/original/pngtree-dining-table-and-chairs-isolated-dining-table-photo-png-image_13735733.png",
+            heading:"Dining Table",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"250",
+
+        },
+        {
+            id:9,
+            image:"https://png.pngtree.com/png-vector/20240914/ourlarge/pngtree-creative-office-chair-icon-furniture-clipart-with-transparent-background-png-image_13833205.png",
+            heading:"Office Chair",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"250",
+
+        },
+        {
+            id:10,
+            image:"https://static.vecteezy.com/system/resources/previews/053/814/942/non_2x/office-chairs-side-view-full-body-isolate-on-transparent-background-png.png",
+             heading:"Office Chair",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"250",
+
+        },
+        {
+            id:11,
+            image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5EEAX1tKhFzFwXUv7MRTWtcvSaiRxhz1cYw&s",
+             heading:"Cup Board",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"250",
+
+        },
+        {
+            id:12,
+            image:"https://thumbs.dreamstime.com/b/wooden-wardrobe-isolated-white-transparent-background-closed-brown-close-up-front-view-graphic-design-element-theme-312240807.jpg",
+             heading:"Cup Board",
+            rating:"4.3",
+            color:"Black Blue Grey",
+            price:"250",
+
+        },
+    ]
+
+    let cart=JSON.parse(localStorage.getItem("cart")) || []
+    let counter=document.getElementById("counter")
+    let card=document.getElementById("card")
+    let submitbtn=document.getElementById("sbt")
+    let mainsection=document.getElementById("main")
+    let searchproducts=document.getElementById("searchproducts")
+   
+
+ function getStars(rating){
+
+    let star=""
+
+    for(i = 0;i < parseInt(rating);i++){
+        star += `<i class="ri-star-s-fill"></i>`
+    }
+
+    if(rating - parseInt(rating) != 0){
+        star+=`<i class="ri-star-half-line"></i>`
+    }
+    return star
+}
+
+function addToCart(id){
+
+    let cartIndex=cart.findIndex((product)=>{
+            return product.id == id
+    })
+
+    if (cartIndex == -1){
+        let productToadd=procuctlist.find((product)=>{
+            return product.id == id
+        })
+        cart.push(productToadd)
+        productToadd.quantity=1
+    }
+    else{
+        cart[cartIndex].quantity++
+    }
+
+    localStorage.setItem("cart",JSON.stringify(cart))
+    updateCounter()
+}
+
+function displayProducts(){
+
+        procuctlist.forEach((product,idx)=>{
+            let deletedprice= product.delprice ? `<del>$${product.delprice} </del>` : ""
+            card.innerHTML += `
+            <div class="col-lg-4">
+                        <img class="img-fluid" src="${product.image}">
+                        <h3>${product.heading}</h3>
+                        <h4 class="text-primary">$${product.price}
+                        <span class="text-danger">${deletedprice}</span>
+                        </h4>
+                        <h4 class="text-warning">${getStars(product.rating)}
+                        <span class="text-primary">${product.rating}</span>
+                        </h4>
+                        <button class="btn btn-warning" onclick="addToCart(${product.id})">Shop Now</button>
+            </div>
+                        `
+        })
+}
+
+function updateCounter(){
+    counter.innerHTML = cart.length
+}
+
+displayProducts()
+updateCounter()
+
+submitbtn.addEventListener("click",function(e){
+    e.preventDefault()
+     let search=document.getElementById("search").value.toLowerCase()
+    mainsection.classList.add("d-none")
+    findproducts.classList.remove("d-none")
+    let newproducts=procuctlist.filter((product)=>{
+        return product.heading.toLowerCase().includes(search)
+    })
+    searchproducts.innerHTML=""
+    newproducts.forEach((product)=>{
+            let deletedprice= product.delprice ? `<del>$${product.delprice} </del>` : ""
+            searchproducts.innerHTML += `
+            <div class="col-lg-4">
+                        <img class="img-fluid" src="${product.image}">
+                        <h3>${product.heading}</h3>
+                        <h4 class="text-primary">$${product.price}
+                        <span class="text-danger">${deletedprice}</span>
+                        </h4>
+                        <h4 class="text-warning">${getStars(product.rating)}
+                        <span class="text-primary">${product.rating}</span>
+                        </h4>
+                        <button class="btn btn-warning" onclick="addToCart(${product.id})">Shop Now</button>
+                       </div> `
+    })
+})
